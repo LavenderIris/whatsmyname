c606d6187d906824a01c00ab88cd7adbc4356698
Priscilla Finder <priscilla@Priscillas-MacBook-Pro.local>
2017-09-06 16:34:16 -0700

    whatsmyname done

---
+++ whats.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("myform.html")
+
+@app.route('/process', methods=['POST'])
+def create_user():
+    name = request.form['name']
+    print name
+
+   # redirects back to the '/' route
+    return redirect('/')
+
+app.run(debug=True)
